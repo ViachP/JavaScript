393b838036ed6f8cfaf323e6109a74923524dcc3
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-05-10 20:45:39 +0300

    feat: add fuctionality (app.js and data.js) for project

---
+++ Dark Mode/data.js
@@ -0,0 +1,30 @@
+const articles = [
+  {
+    id: 1,
+    title: "How to improve your English vocabulary easily",
+    date: new Date(2022, 3, 29),
+    length: 4,
+    snippet: `Acquiring vocabulary is probably the essential thing foreign language learners should do. Not only because knowing a lot of words makes you sound smart but also because words and phrases are the building blocks...`,
+  },
+  {
+    id: 2,
+    title: "You just need 20 hours to learn anything!",
+    date: new Date(2022, 1, 27),
+    length: 4,
+    snippet: `No you don’t need to be a superhuman for this — Need to learn a new programming framework for your new project or trying to learn to play the piano or learning how to solve a rubix cube. Learning doesn’t need to be...`,
+  },
+  {
+    id: 3,
+    title: "22 CSS Libraries For Website Design",
+    date: new Date(2021, 11, 31),
+    length: 4,
+    snippet: `Today we are going to learn about useful CSS libraries for website design and development. 1) Animate.css Help us create animation effects using HTML CSS for the website. Link 2) normalize.css Normalize.css is a library...`,
+  },
+  {
+    id: 4,
+    title: "Benefits of Using MERN Stack",
+    date: new Date(2021, 10, 18),
+    length: 13,
+    snippet: `Why You Should Use MERN Stack for Startup Apps? — I’ve been developing with the MERN stack for over five years, so I wanted to put together a guide on why this particular platform is so beneficial when...`,
+  },
+];
